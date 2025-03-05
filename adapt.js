0d3888bfe6f6080931bf169eac2d8feb51642b6b
A-Sal3 <asalinas3205@gmail.com>
2025-03-05 11:19:45 -0600

    Renamed util.js -> utils.js

--- adapt.js
+++ adapt.js
@@ -1,6 +1,6 @@
-import {
-    setClicked
-} from './song.js';
+import { setClicked } from './song.js';
+import { nextChord } from './utils.js';
+
 const initSqlJs = window.initSqlJs;
 const SQL = await initSqlJs({
     locateFile: file => `https://sql.js.org/dist/${file}`
@@ -8,8 +8,7 @@ const SQL = await initSqlJs({
 let xhr = new XMLHttpRequest();
 xhr.responseType = 'arraybuffer';
 
-let curr;
-let noteI;
+let noteI, curr;
 function setCurr(c) {
     noteI = c[0]
     curr = c[1];
@@ -24,9 +23,9 @@ async function click() {
         let db = new SQL.Database(uInt8Array);
         let contents = db.exec(`SELECT Next, Type, D.Dist, n1, n2, n3, n4 FROM ChordProg C, DistTable D 
             WHERE Curr = ? AND D.Dist = 1 AND C.ProgID = D.ProgID 
-            ORDER BY RANDOM() LIMIT 1`, [curr]);
+            ORDER BY RANDOM() LIMIT 1`, [curr])[0].values[0];
         
-        console.log(contents[0].values[0]);
+        console.log(contents);
     };
     xhr.send();
 }
