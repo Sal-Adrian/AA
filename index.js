5d30acfb11fbed2aab91ad34f9889e2c2ecc41cb
A-Sal3 <asalinas3205@gmail.com>
2025-03-10 15:58:45 -0500

    Prepared environment for game music

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 import {
-    drumLoop,
     playSong,
     testMessage
 } from './song.js';
-import {
-    click
-} from './adapt.js';
-
-
-const beatButton = document.getElementById("beatBtn");
-beatButton.addEventListener('click', async function () {
-    await Tone.start();
-    drumLoop();
-});
-
-const songButton = document.getElementById("songBtn");
-songButton.addEventListener('click', async function () {
-    await Tone.start();
-    playSong();
-});
-
-const clickButton = document.getElementById("clickBtn");
-clickButton.addEventListener('click', async function () {
-    click();
-});
-
 
 function mainTest() {
     testMessage();
 }
 mainTest();
+// await Tone.start();
+// playSong();
